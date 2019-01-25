0476f21fc522a95398fbe7cf9e9c4e24f4363907
fzz0826 <1921008264@qq.com>
2019-01-25 17:53:18 +0800

    创建项目

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        这个是一个小项目 
+      </div>
+    );
+  }
+}
+
+export default App;
