cd20896b288d2bceb9cb123a3100626e1af0b7b2
fzz0826 <1921008264@qq.com>
2019-01-26 16:16:27 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Router from './router'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        这个是一个小项目 
-      </div>
+      <Router></Router>
     );
   }
 }
